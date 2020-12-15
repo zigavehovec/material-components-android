f2e475675e74e5195e85d0f30770f4469ac7b685
Žiga Vehovec <ziga.veho@gmail.com>
2020-12-15 18:32:01 +0100

    Revert "[BaseTransientBottomBar] Check for nullable AccessibilityService"
    This reverts commit 2b95793e

--- lib/java/com/google/android/material/snackbar/BaseTransientBottomBar.java
+++ lib/java/com/google/android/material/snackbar/BaseTransientBottomBar.java
@@ -1104,9 +1104,6 @@ public abstract class BaseTransientBottomBar<B extends BaseTransientBottomBar<B>
 
   /** Returns true if we should animate the Snackbar view in/out. */
   boolean shouldAnimate() {
-    if (accessibilityManager == null) {
-      return true;
-    }
     int feedbackFlags = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
     List<AccessibilityServiceInfo> serviceList =
         accessibilityManager.getEnabledAccessibilityServiceList(feedbackFlags);
